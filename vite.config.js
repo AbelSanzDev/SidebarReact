2fe54ba6c197d57e19b82242f864eecc1ee7eb95
Abel Sánchez <abelsandev@gmail.com>
2024-06-22 17:09:41 -0700

    Se realizaron cambios

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      name: 'MyReactLibrary',
+      fileName: (format) => `sidebar-react-pro.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
